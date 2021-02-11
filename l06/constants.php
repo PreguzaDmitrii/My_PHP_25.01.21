338c53d4f3aba00096ad244856a189c5a2ad6598
PreguzaDmitrii <preguzadmitrii@gmail.com>
2021-02-11 20:03:04 +0200

    Added: Folder constants.php

---
+++ l06/constants.php
@@ -0,0 +1,14 @@
+<?php
+
+define('APP_VERSION', '3.0.1');
+
+echo APP_VERSION, PHP_EOL;
+
+const POWER = '220v';
+
+echo POWER, PHP_EOL;
+
+const MY_COEFICENT = 3.1;
+$salary = 2000;
+
+echo $salary * MY_COEFICENT, PHP_EOL;
