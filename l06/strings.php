4884aa48f26f6b521896334f37a684bf74d3dfdb
PreguzaDmitrii <preguzadmitrii@gmail.com>
2021-02-11 19:42:53 +0200

    Added: Row rules

--- l06/strings.php
+++ l06/strings.php
@@ -2,3 +2,29 @@
 echo '1';
 print 'ooo';
 print PHP_EOL;
+
+$name = 'Dmitrii';
+echo 'Hello $name\n', PHP_EOL;
+echo "Hello user with name \"{$name}\"\n";
+
+$nowdoc = <<<'THML'
+<script type="text/javascript">
+    alert(1);
+</script>
+THML;
+
+$heredoc = <<<THML
+<script type="text/javascript">
+    alert(1);
+</script>
+THML;
+
+echo $nowdoc, PHP_EOL;
+echo $heredoc, PHP_EOL;
+
+
+
+
+
+
+
