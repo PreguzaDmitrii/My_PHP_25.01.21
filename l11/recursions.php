f5852a2e0c848c051fa75d7ea529a53ce296ecad
PreguzaDmitrii <preguzadmitrii@gmail.com>
2021-03-02 19:12:55 +0300

    Add recursions.php file

---
+++ l11/recursions.php
@@ -0,0 +1,21 @@
+<?php
+
+//function rec()
+//{
+//    echo mt_rand(). '>>' . memory_get_usage(). ' bytes used' . PHP_EOL;
+//    sleep(1);
+//
+//    rec();
+//}
+//rec();
+
+function power(int $number, $power)
+{
+
+}
+
+
+
+
+
+
